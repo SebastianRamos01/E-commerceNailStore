eaa4c8d5f63b0437f078ec36c0e4c94b689e50e7
SebastianRamos01 <sebasr.sr@hotmail.com>
2023-06-20 17:55:19 -0300

    Vistas restantes

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -1,8 +1,18 @@
 const path = require("path")
+const db = require("../database/models");
 
 const mainController = {
     main: (req,res) => {
-        res.render(path.resolve(__dirname, "../views/home.ejs"));
+        db.Products.findAll()
+        .then(data => {
+            res.render(path.resolve(__dirname, "../views/home.ejs"), { allProducts: data })
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    },
+    aboutUs: (req, res) => {
+        res.render(path.resolve(__dirname, "../views/aboutUs.ejs"))
     }
 };
 
--- routers/mainRouters.js
+++ routers/mainRouters.js
@@ -1,9 +1,9 @@
 const express = require("express");
-
 const {mainController} = require("../controllers/mainControllers");
 
 const mainRouter = express.Router(); 
 
 mainRouter.get("/home", mainController.main);
+mainRouter.get("/about-us", mainController.aboutUs)
 
 module.exports = mainRouter
