e255c971cb32622aee00810c62e1cfc297f83590
SebastianRamos01 <sebasr.sr@hotmail.com>
2023-06-30 20:14:26 -0300

    Configuracion

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //SERVER
 const express = require("express")
 const app = express();
-
+require("dotenv").config()
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Ejecutado en el puerto " + port));
